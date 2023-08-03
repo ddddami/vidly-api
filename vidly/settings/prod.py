d9c6a9bdeb518b2dcf4baaefbbab0d455fd3bfca
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-08-03 02:19:49 +0100

    Add database connection string for prod

--- vidly/settings/prod.py
+++ vidly/settings/prod.py
@@ -1,10 +1,14 @@
 import os
+import dj_database_url
 from .common import *
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # DATABASES
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+}
